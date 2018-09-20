3598217e92146422f4406d761524c3dbb41f08d2
saranyamandava <37849027+saranyamandava@users.noreply.github.com>
2018-09-20 10:37:17 -0500

    Update test_xyHacker.js

--- test_xyHacker.js
+++ test_xyHacker.js
@@ -1,16 +1,25 @@
 
 async function start(){
+	   img = document.getElementById('image').files[0];
 	
-	model = await tf.loadModel('model/model.json')
+        console.log(img)
+        
+        model = await tf.loadModel('model/model.json')
         
         var status = document.getElementById('status')
-      
+        console.log(status)
         status.innerHTML = 'Model Loaded'
+        console.log(status)
+        //document.getElementById('status').innerHTML = 'Model Loaded';
+
         
-        var myText = document.getElementById("myText");
-        var s = myText.value;
-        console.log(s)
-        
-         
-        }
-        
+        predict(img)
+    
+        //load the class names
+        await loadDict()
+        console.log('img')
+   
+
+				}	
+
+					  
